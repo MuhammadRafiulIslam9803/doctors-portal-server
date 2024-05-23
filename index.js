c6c1481515720e58a971ef9c6fabd0ea91920974
Rafiul Islam <www.rafiulislam1998@gmail.com>
2024-05-23 08:55:57 +0600

    update all

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const jwt = require('jsonwebtoken');
 const app = express()
 require('dotenv').config()
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 5000
 //middleware
 app.use(cors())
@@ -51,17 +52,19 @@ async function run() {
     const bookingsCollection = client.db('doctorsPortal').collection('bookings')
     const usersCollection = client.db('doctorsPortal').collection('users');
     const doctorsCollection = client.db('doctorsPortal').collection('doctors');
+    const paymentsCollection = client.db('doctorsPortal').collection('payments');
 
-    const verifyAdmin = async (req, res, next) =>{
+
+    const verifyAdmin = async (req, res, next) => {
       const decodedEmail = req.decoded.email;
       const query = { email: decodedEmail };
       const user = await usersCollection.findOne(query);
 
       if (user?.role !== 'admin') {
-          return res.status(403).send({ message: 'forbidden access' })
+        return res.status(403).send({ message: 'forbidden access' })
       }
       next();
-  }
+    }
 
 
     app.get('/appointment', async (req, res) => {
@@ -119,6 +122,46 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/bookings/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const booking = await bookingsCollection.findOne(query);
+      res.send(booking);
+    })
+
+    app.post('/create-payment-intent', async (req, res) => {
+      const booking = req.body;
+      const price = booking.price;
+      const amount = price * 100;
+
+      const paymentIntent = await stripe.paymentIntents.create({
+        currency: 'usd',
+        amount: amount,
+        "payment_method_types": [
+          "card"
+        ]
+      });
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
+
+    app.post('/payments', async (req, res) =>{
+      const payment = req.body;
+      const result = await paymentsCollection.insertOne(payment);
+      const id = payment.bookingId
+      const filter = {_id: new ObjectId(id)}
+      const updatedDoc = {
+          $set: {
+              paid: true,
+              transactionId: payment.transactionId
+          }
+      }
+      const updatedResult = await bookingsCollection.updateOne(filter, updatedDoc)
+      res.send(result);
+  })
+
+
     app.get('/jwt', async (req, res) => {
       const email = req.query.email;
       const query = { email: email };
@@ -150,36 +193,48 @@ async function run() {
     });
 
     app.put('/users/admin/:id', verifyJWT, verifyAdmin, async (req, res) => {
-            const id = req.params.id;
-            const filter = { _id: new ObjectId(id) }
-            const options = { upsert: true };
-            const updatedDoc = {
-                $set: {
-                    role: 'admin'
-                }
-            }
-            const result = await usersCollection.updateOne(filter, updatedDoc, options);
-            res.send(result);
-        });
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) }
+      const options = { upsert: true };
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updatedDoc, options);
+      res.send(result);
+    });
 
-    app.get('/doctors',verifyJWT,verifyAdmin, async (req, res) => {
+    // app.get('/addPrice', async (req, res) => {
+    //     const filter = {}
+    //     const options = { upsert: true }
+    //     const updatedDoc = {
+    //         $set: {
+    //             price: 99
+    //         }
+    //     }
+    //     const result = await appointmentCollection.updateMany(filter, updatedDoc, options);
+    //     res.send(result);
+    // })
+
+    app.get('/doctors', verifyJWT, verifyAdmin, async (req, res) => {
       const query = {};
       const doctors = await doctorsCollection.find(query).toArray();
       res.send(doctors);
     })
 
-    app.post('/doctors',verifyJWT,verifyAdmin, async (req, res) => {
+    app.post('/doctors', verifyJWT, verifyAdmin, async (req, res) => {
       const doctor = req.body;
       const result = await doctorsCollection.insertOne(doctor);
       res.send(result);
     });
 
-    app.delete('/doctors/:id',verifyJWT,verifyAdmin, async (req, res) => {
+    app.delete('/doctors/:id', verifyJWT, verifyAdmin, async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const result = await doctorsCollection.deleteOne(filter);
       res.send(result);
-  })
+    })
 
 
   }
